a584ea0a5c37eeb6e01cdeb7dfd753b991f91651
Jim Eli <31859133+JimEli@users.noreply.github.com>
2022-03-28 08:06:03 -0600

    Update freeRTOS-main.c

--- FreeRTOS Demo/freeRTOS-main.c
+++ FreeRTOS Demo/freeRTOS-main.c
@@ -15,54 +15,56 @@ void redThread(void* arg1)
     RedToggle(); 
     vTaskDelay(DELAY_MS); 
   } 
-	vTaskDelete(NULL);
+  vTaskDelete(NULL);
 }
+
 void blueThread(void* arg1) { while(1) { BlueToggle(); vTaskDelay(DELAY_MS/2); } vTaskDelete(NULL); }
+
 void greenThread(void* arg1) { while(1) { GreenToggle(); vTaskDelay(DELAY_MS/4); } vTaskDelete(NULL); }
+
 void orangeThread(void* arg1) { while(1) { OrangeToggle(); vTaskDelay(DELAY_MS/8); } vTaskDelete(NULL); }
 
 int main(void) 
 {
-	GPIO_Init();
+  GPIO_Init();
 	
   xTaskCreate(
-	            blueThread,        /* task function */
-	            "blue led toggle", /* description */
-	            100,               /* stack size */
+                blueThread,        /* task function */
+                "blue led toggle", /* description */
+                100,               /* stack size */
+            	NULL,              /* parameters */
+            	1,                 /* prioirity */
+            	NULL               /* handle */
+             );
+
+  xTaskCreate(
+                redThread,         /* task function */
+                "red led toggle",  /* description */
+                100,               /* stack size */
              	NULL,              /* parameters */
             	1,                 /* prioirity */
             	NULL               /* handle */
              );
 
   xTaskCreate(
-	            redThread,         /* task function */
-	            "red led toggle",  /* description */
-	            100,               /* stack size */
-             	NULL,              /* parameters */
-            	1,                 /* prioirity */
-            	NULL               /* handle */
-             );
-
-  xTaskCreate(
-	            orangeThread,        /* task function */
-	            "orange led toggle", /* description */
-	            100,                 /* stack size */
+                orangeThread,        /* task function */
+                "orange led toggle", /* description */
+                100,                 /* stack size */
              	NULL,                /* parameters */
             	1,                   /* prioirity */
             	NULL                 /* handle */
              );
 
   xTaskCreate(
-	            greenThread,        /* task function */
-	            "green led toggle", /* description */
-	            100,                /* stack size */
+                greenThread,        /* task function */
+                "green led toggle", /* description */
+                100,                /* stack size */
              	NULL,               /* parameters */
             	1,                  /* prioirity */
             	NULL                /* handle */
              );
 
-  vTaskStartScheduler();
-	
-	while(1);
+  vTaskStartScheduler();	
+  while(1);
 }	
 
